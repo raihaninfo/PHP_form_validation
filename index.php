861c56e793338faee0dca817749a3debd2872043
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-01-25 18:47:05 +0600

    use cdn

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <title>Learning PHP</title>
 </head>
@@ -136,7 +136,7 @@
 
 
 
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
