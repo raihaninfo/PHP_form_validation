8a0d58494ccd1cd28b49072da5da09149cbb6c15
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-01-25 18:58:59 +0600

    title change

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title>Learning PHP</title>
+    <title>PHP form validation</title>
 </head>
 
 <body>
